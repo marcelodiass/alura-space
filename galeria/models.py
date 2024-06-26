03bbc92f88b60c564e5b55ecfbff5805b99fa5da
marcelodiass <marcelodias.desenvolvedor@gmail.com>
2024-06-25 21:46:59 -0300

    incrementando a data no model fotografia

--- galeria/models.py
+++ galeria/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 class Fotografia(models.Model):
     OPCOES_CATEGORIA = [
@@ -14,6 +15,7 @@ class Fotografia(models.Model):
     descricao = models.TextField(null=False, blank=False)
     foto = models.CharField(max_length=100, null=False, blank=False)
     publicada = models.BooleanField(default=False)
+    data_fotografia = models.DateTimeField(default=datetime.now, blank=False)
     
     def __str__(self):  
         return f"Fotografia: [nome={self.nome}]"
--- galeria/views.py
+++ galeria/views.py
@@ -3,7 +3,7 @@ from galeria.models import Fotografia
 
 
 def index(request):
-    fotografias = Fotografia.objects.filter(publicada=True)
+    fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)
     
     return render(request, 'galeria/index.html', {'cards': fotografias})
 
