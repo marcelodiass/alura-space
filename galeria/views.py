20ce559d65ca76f64073e375b7b4f5f2b465e19d
marcelodiass <marcelodias.desenvolvedor@gmail.com>
2024-08-01 23:29:52 -0300

    validação da home e da busca

--- galeria/views.py
+++ galeria/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from galeria.models import Fotografia
+from django.contrib import messages
 
 
 def index(request):
+    if not request.user.is_authenticated:
+        messages.error(request, 'Usuário não está logado!')
+        return redirect('login')
+        
     fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)
-    
     return render(request, 'galeria/index.html', {'cards': fotografias})
 
 
@@ -13,6 +17,10 @@ def imagem(request, foto_id):
     return render(request, 'galeria/imagem.html', {'fotografia': fotografia})
 
 def buscar(request):
+    if not request.user.is_authenticated:
+        messages.error(request, 'Usuário não está logado!')
+        return redirect('login')
+    
     fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)
     
     if "buscar" in request.GET:
